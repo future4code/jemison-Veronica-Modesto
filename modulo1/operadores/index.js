c64526cae869d6e39daabafb3ada9a57ceae67b8
Veronica Dalcolmo Modesto <vevedm@gmail.com>
2022-04-15 18:18:34 -0300

    operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,104 @@
+// Exercicio de interpretacao
+
+// 1. 
+
+/* a - false
+b - false
+c - true
+d - booleano */
+
+//2. 
+/* let primeiroNumero  = Number( prompt("Digite um numero!"))
+let segundoNumero = Number( prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) */
+
+
+
+//EXERCICIO DE ESCRITA
+
+//1.
+//let idade = Number( prompt ("Qual a sua idade?"))
+/* let amigaidade = Number( prompt ("A idade da minha melhor amiga é"))
+let minhaidade = Number( prompt ("A minha idade é"))
+
+let resultado = minhaidade > amigaidade
+resposta - TRUE
+
+//resultado = minhaidade - amigaidade
+console.log(resultado) */
+
+
+//2. 
+/* let numeropar  = Number( prompt("Digite um numero par!"))
+
+let resto = numeropar % 2
+
+console.log(resto)
+ */
+   //O padrão é que todos os números PAR o resto é 0
+
+//Com número impar:
+
+/* let numeroimpar = Number( prompt("Digite um numero impar!"))
+
+let resto = numeroimpar % 2
+
+console.log(resto) */
+
+//O padrão é que todo os número IMPAR o resto é 1
+
+//3. Faça um programa que pergunte ao usuário sua idade em anos. Depois, imprima no console 
+ 
+/* A)
+
+let idadeuser = Number( prompt ("Qual a sua idade?"))    
+let resultadomeses = idadeuser * 12
+
+console.log(resultadomeses) */ 
+
+//RESPOSTA  = 408
+
+//B)
+
+/* let idadeuser = Number( prompt ("Qual a sua idade?"))    
+let resultadoemdias = idadeuser * 365
+
+console.log(resultadoemdias)
+
+RESPOSTA = dias 12410 */
+
+//C)
+
+/* let idadeuser = Number( prompt ("Qual a sua idade?"))    
+let resultadoemhoras = idadeuser * 365 * 24
+
+console.log(resultadoemhoras)
+
+RESPOSTA = 297840 horas */
+
+//4. Peguei os numero 11 e 10 respectivamente.
+
+/* let numero1 = Number( prompt ("Fale um número"))  
+let numero2 = Number( prompt ("Fale outro número")) 
+
+
+let maiorque = numero1 > numero2 
+let igualque = numero1 === numero2 
+let divisivel = numero1 / numero2
+let divisivel2 = numero2 / numero1 */
+
+//console.log(maiorque) true
+
+//console.log(igualque) false
+
+//console.log(divisivel) false
+
+//console.log(divisivel2) false 
+
+
+
+
+    
