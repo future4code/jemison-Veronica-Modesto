c23395711f002be1bbb66ccdbab3ec43dfbd16fb
Veronica Dalcolmo Modesto <vevedm@gmail.com>
2022-04-19 21:09:25 -0300

    exercicios-arrays

---
+++ modulo1/string-arrays/index.js
@@ -0,0 +1,78 @@
+//EXERCICIOS DE INTERPRETACAO
+
+/* let array
+console.log('a. ', array)
+
+   //resposta = undefined - pq a variável nao recebeu nenhum valor
+
+array = null
+console.log('b. ', array)
+
+   //reposta = null
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+   //resposta = 11 
+
+let i = 0
+console.log('d. ', array[i])
+  // resposta = 3
+
+array[i+1] = 19
+console.log('e. ', array)
+   // resposta = [3, 19 , 5 .... ]
+                  0   1   2
+
+const valor = array[i+6]
+console.log('f. ', valor)
+   // resposta: valor = 9 */
+
+/* const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length */
+   //resposta - SUBI NUM ÔNIBUS EM MIRROCOS
+   //length = 27
+
+
+   // EXERCICIOS DE ESCRITA
+
+   /* 1-
+
+   let nomeDoUsuario = prompt ("Qual é o seu nome? ")
+   let emailDoUsuario = prompt ("Qual é o seu e-mail? ")
+
+   console.log("O e-mail", emailDoUsuario, "foi cadastrado com sucesso. Seja bem-vinda(o),",nomeDoUsuario,".") */
+
+   // 2- 
+
+/*    let comidas = [
+   "lasanha", 
+   "mexicana", 
+   "japonesa", 
+   "brasileira", 
+   "churrasco"]
+
+   //console.log( "Essas são as minhas comidas preferidas:",comidas )
+
+   let comidaUser = prompt("fale a sua comida favorita" )
+   
+   comidas.splice(2, 1)
+   comida.push(comidaUser)
+   console.log(comidaUser) */
+
+  
+   //3 - 
+ /* 
+  let tarefas = []
+  tarefas [0] = prompt("Qual a primeira tarefa que voce faz no dia?")
+  tarefas [1] = prompt("Qual a segunda tarefa que voce faz no dia?")
+  tarefas [2] = prompt("Qual a terceira tarefa que voce faz no dia?")
+
+  //console.log(tarefas)
+
+  let indice = prompt("qual tarefa vc ja realizou hoje? Coloque de 0,1 ou 2")
+  
+  tarefas.splice(indice, 0)
+  
+  console.log(tarefas) */
+
