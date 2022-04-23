bb220281890fa4ccb8a5f89f6e8cb36b9c9c5874
Veronica Dalcolmo Modesto <vevedm@gmail.com>
2022-04-22 21:09:16 -0300

    projeto-ainda-nao-terminado

---
+++ modulo1/projeto-lista-js/exercicios.js
@@ -0,0 +1,129 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+function soma(num1, num2) {
+  // implemente sua lógica aqui
+  return num1 + num2
+}
+
+// EXERCÍCIO 0B
+function imprimeMensagem() {
+  // implemente sua lógica aqui
+  const mensagem = prompt('Digite uma mensagem!')
+
+  console.log(mensagem)
+}
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+function calculaAreaRetangulo() {
+
+  let altura = Number( prompt( "Qual a altura do retângulo?"))
+  let largura = Number( prompt( "Qual a largura do retângulo?"))
+  
+  let calculaAreaRetangulo = altura * largura  
+  console.log(calculaAreaRetangulo)
+
+}
+
+// EXERCÍCIO 02
+function imprimeIdade() {
+
+  let anoAtual = Number( prompt( "Qual o ano atual?"))
+  let anoNascimento = Number( prompt( "Qual o ano do seu nascimento?"))
+  
+  let imprimeIdade = anoAtual - anoNascimento 
+ 
+  console.log(imprimeIdade)
+}
+
+// EXERCÍCIO 03
+function calculaIMC(peso, altura) {
+ const IMC = peso / (altura * altura)
+ 
+ console.log(IMC)
+}
+calculaIMC(85, 1.8)
+
+
+// EXERCÍCIO 04
+function imprimeInformacoesUsuario() {
+
+  let nomePessoa = prompt ( "Qual o seu nome?" )
+  let idadeUser = Number( prompt ( "Qual a sua idade?" ))
+  let email = prompt ( "Qual o seu e-mail?" )
+
+  //let imprimeInformacoesUsuario = 
+
+  console.log("Meu nome é",nomePessoa,", tenho",idadeUser,"anos, e o meu e-mail é",email)
+ 
+  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+
+}
+
+// EXERCÍCIO 05
+function imprimeTresCoresFavoritas() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 06
+function retornaStringEmMaiuscula(string) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 07
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 08
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 09
+function retornaPrimeiroElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 10
+function retornaUltimoElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}
