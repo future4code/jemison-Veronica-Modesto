0fc3979ddaf4559dd245175449054f964b1b85f1
Veronica Dalcolmo Modesto <vevedm@gmail.com>
2022-04-26 21:55:40 -0300

    exercicio-objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,91 @@
+//EXERCICIOS DE INTERPRETACAO
+
+//1 - a
+// Nachtergaele
+// Virginia Cavendish
+// canal:"Telecine", horario: "21h"
+
+//2 - 
+//a cachorro:
+   /*  nome: "Juca", 
+	idade: 3, 
+	raca: "SRD"
+
+    gato: 
+    nome: "Juba", 
+	idade: 3, 
+	raca: "SRD"
+
+    tartaruga:
+    nome: "Jubo", 
+	idade: 3, 
+	raca: "SRD" */
+
+//b - Pega os elementos do outro objeto para adicionar ou substituir os elementos de outro objeto.
+
+/* 3 - 
+
+a) False - undefined
+
+b) Por que a chave ''altura' nao está definida.
+
+EXERCICIOS DE ESCRITA
+
+1 - Crie um objeto. Ele deve conter duas propriedades: nome (string) e apelidos 
+(um array que sempre terá exatamente três apelidos). Depois, escreva uma função que 
+recebe como entrada um objeto e imprime uma mensagem no modelo abaixo: */
+
+//"Eu sou",eu.nome,"mas pode me chamar de:",eu.apelido""
+
+
+/* const ve = {
+    nome: "veronica",
+    apelido: ["veve", "ve", "vevis"]
+}
+const emilia = {
+    nome: "emilia",
+    apelido: ["em", "mimi", "milia"]
+}
+
+function jeitosDeMeChamar (pessoa){
+    console.log("Eu sou",pessoa.nome,"mas pode me chamar de:",pessoa.apelido[0],pessoa.apelido[1],"ou", pessoa.apelido[2])
+}
+jeitosDeMeChamar(ve) 
+jeitosDeMeChamar(emilia)  */
+
+
+//2
+/* 
+const ve = {
+    nome: "veronica",
+    idade: 34,
+    profissao: "estudante"
+}
+const emilia = {
+    nome: "emilia",
+    idade: 92,
+    profissao: "faço programa"
+}
+
+function analisePessoa (pessoa){
+    return [pessoa.nome, pessoa.nome.length, pessoa.idade, pessoa.profissao, pessoa.profissao.length]
+}
+
+console.log (analisePessoa(ve))
+console.log (analisePessoa(emilia)) */
+
+//3 -  
+
+    /* const carrinho = []
+    
+    const pera = {nome: "pera", disponibilidade: true}
+    const banana = {nome: "banana", disponibilidade: true}
+    const uva = {nome: "uva", disponibilidade: true}
+
+    function addCarrinho (fruta1, fruta2, fruta3){
+        carrinho.push(fruta1, fruta2, fruta3)
+    }
+addCarrinho(pera, banana, uva)
+console.log(carrinho)
+ */
+        
