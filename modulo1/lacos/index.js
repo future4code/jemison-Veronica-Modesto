2d1422043da153a3af1291b66e7015495a97a7c9
Veronica Dalcolmo Modesto <vevedm@gmail.com>
2022-05-09 00:27:55 -0300

    lacos

--- modulo1/lacos/index.js
+++ modulo1/lacos/index.js
@@ -22,26 +22,30 @@ if (animais === 0) {
 } else if (animais > 0) {
 
     const nomesBichinhos = []
+
     //enquanto o NOME for menor que o numero de animais
+
     for (let nome = 1; nome <= animais; nome++) {  
         const nomeDoBixinho = prompt(`Qual o nome do seu bixinho ${nome} ?`)
         nomesBichinhos.push(nomeDoBixinho)
-        console.log(nomesBichinhos)
-    }
+        console.log(nomesBichinhos)    }
 
 } else { 
     console.log("Informe um número válido")
 
 
-2. Considere que você tenha acesso a um `array`  (chamado de 'array original') que é composto somente de números. Baseando-se nisso, crie uma função para cada um dos itens abaixo, realizando as operações pedidas:
+2. Considere que você tenha acesso a um `array`  (chamado de 'array original') que é composto somente de números. 
+Baseando-se nisso, crie uma função para cada um dos itens abaixo, realizando as operações pedidas:
     
     a) Escreva um programa que **imprime** cada um dos valores do array original.
     
     b) Escreva um programa que **imprime** cada um dos valores do array original divididos por 10
     
-    c) Escreva um programa que **crie** um novo array contendo, somente, os números pares do array original e **imprima** esse novo array
+    c) Escreva um programa que **crie** um novo array contendo, somente, os números pares do array original e
+     **imprima** esse novo array
     
-    d) Escreva um programa que **crie** um novo array contendo strings, da seguinte forma: "O elemento do índex `i` é: `numero`". Depois, **imprima** este novo array.
+    d) Escreva um programa que **crie** um novo array contendo strings, da seguinte forma: "O elemento do índex 
+    `i` é: `numero`". Depois, **imprima** este novo array.
     
     e) Escreva um programa que imprima no console o maior e o menor números contidos no array original */
 
@@ -68,16 +72,48 @@ imprimeValores (original) */
     }
 imprimeValores (original) */
 
-//c - 
+//c - Escreva um programa que **crie** um novo array contendo, somente, os números pares do array original e
+    // **imprima** esse novo array
 
-/* const novoArray = [2, 28, 74]
-    console.log(novoArray) */
+    
+/* const original = [11, 35, 49, 87, 65, 2, 28, 74] 
+const numPares = []
+
+function novoArrayPares (array) {
+    for (let i = 0; i < array.length; i++) {
+        let pares = (array[i] % 2 === 0)
+        console.log(pares.push(array[i]))
+    }
+}  
+novoArrayPares ()    */  
+/* d - 
+
+const novoArrayStrings = ["O elemento do índex `i` é: `numero`"] */
+
+//e - Escreva um programa que imprima no console o maior e o menor números contidos no array original
 
 
-//d - 
+/* const original = [11, 35, 49, 87, 65, 2, 28, 74] 
 
-const novoArrayStrings = ["O elemento do índex `i` é: `numero`"]
+function MaiorNumero (array){
+    let maior = 0
+    for(let numero of array) {
+        if (numero > maior){
+            maior = numero
+        }
+    }
+    return maior
+}
+console.log(MaiorNumero(original))
 
 
-
-
+function MenorNumero (array){
+    let menor = 0
+    for(let numero of array) {
+        if (numero < menor){
+            menor = numero
+        }
+    }
+    return menor
+}
+console.log(MenorNumero(original)) */
