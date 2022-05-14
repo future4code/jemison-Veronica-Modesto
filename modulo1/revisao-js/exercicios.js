6c2f87eeb7f6c2a43bd6fe347c699919d05176f2
Veronica Dalcolmo Modesto <vevedm@gmail.com>
2022-05-13 21:29:31 -0300

    exercicios-JS

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -17,8 +17,7 @@ retornaArrayInvertido(1,2,3)
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-   let arrayOrdenado = array.sort();
-   return arrayOrdenado
+  return array.sort((a, b) => a - b)
 }
 retornaArrayOrdenado()
 
@@ -40,23 +39,53 @@ function retornaNumerosParesElevadosADois(array) {
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  return 
-}
+  let maior = 0;
+  for (let number of array) {
+     if (number > maior) {
+         maior = number;
+       }
+    }
+    return maior
+  }
+
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-
+  let maiorNum = num2
+  let menorNum = num1
+  
+  if (num1 > num2) {
+     maiorNum = num1
+     menorNum = num2
+   }
+return objeto = { maiorNumero: maiorNum, maiorDivisivelPorMenor: (maiorNum % menorNum === 0), diferenca: (maiorNum - menorNum) }
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
+   let numerosPares = [];
+    for (let i = 0; numerosPares.length < n; i++) {
+        if (i % 2 == 0) {
+            numerosPares.push(i);
+        }
+    }
+    return numerosPares;
+   }     
    
-}
-
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
+   let triangulo 
 
-}
+   if ((ladoA === ladoB) && (ladoA === ladoC)) {
+   triangulo = "Equilátero"
+   }
+   else if ((ladoA === ladoB) || (ladoC === ladoA) || (ladoB === ladoC)) {
+   triangulo = "Isósceles"
+   }
+   else (triangulo = "Escaleno")
+   return triangulo;
+   }   
+
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
@@ -65,22 +94,31 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+   return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
 }
+
+
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+  let outraPessoaAnonima = {
+     ...pessoa,
+     nome: "ANÔNIMO"
+     }
+     return outraPessoaAnonima
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+   return pessoas.filter((item) => 
+   ((item.altura >= 1.5) && ((item.idade > 14) && (item.idade < 60))))
 }
+
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+   return pessoas.filter((item) => 
+   ((item.altura < 1.5) || (item.idade <= 14) || (item.idade >= 60)))
 }
 
 // EXERCÍCIO 14
